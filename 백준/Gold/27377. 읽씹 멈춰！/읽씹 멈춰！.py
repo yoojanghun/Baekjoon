676ba4f54b65ea11f788733793e3f472a1fd9c6f
yoojanghun <157185025+yoojanghun@users.noreply.github.com>
2025-11-08 10:41:45 +0900

    [Gold III] Title: 읽씹 멈춰!, Time: 416 ms, Memory: 111952 KB -BaekjoonHub

---
+++ 백준/Gold/27377. 읽씹 멈춰！/읽씹 멈춰！.py
@@ -0,0 +1,18 @@
+T = int(input())
+
+for i in range(T):
+    n = int(input())
+    s, t = map(int, input().split())  # s: 타이핑, t: 복붙
+    time = 0
+    while n > 0:
+        if n % 2 == 1:
+            n -= 1
+            time += s
+        else:
+            if (n // 2) * s < t:  # 타이핑이 복붙보다 빠름
+                time += (n // 2) * s
+                n //= 2
+            else:                 # 복붙이 타이핑보다 빠름
+                time += t
+                n //= 2
+    print(time)
