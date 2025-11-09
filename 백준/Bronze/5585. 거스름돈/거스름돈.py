77b86dd95409476f8cd176383f1b16861a10af65
yoojanghun <157185025+yoojanghun@users.noreply.github.com>
2025-11-09 13:44:19 +0900

    [Bronze II] Title: 거스름돈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5585. 거스름돈/거스름돈.py
@@ -0,0 +1,25 @@
+pay = int(input())
+change = 1000 - pay
+cnt = 0
+
+while change > 0:
+    if change >= 500:
+        cnt += 1
+        change -= 500
+    elif change >= 100:
+        cnt += 1
+        change -= 100
+    elif change >= 50:
+        cnt += 1
+        change -= 50
+    elif change >= 10:
+        cnt += 1
+        change -= 10
+    elif change >= 5:
+        cnt += 1
+        change -= 5
+    elif change >= 1:
+        cnt += 1
+        change -= 1
+
+print(cnt)
